fb1abfc8df488a3f8ff1f96f40d22c3fc594afde
Arul525 <100610009+Arul525@users.noreply.github.com>
2022-03-02 16:09:42 +0530

    Update dice.sh

--- dice.sh
+++ dice.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+
+dice=$(( (RANDOM%100)+10))
+dice1=$(( (RANDOM%100)+10))
+dice2=$(( (RANDOM%100)+10))
+dice3=$(( (RANDOM%100) +10))
+dice4=$(( (RANDOM%100)+10))
+
+echo $dice
+echo $dice1
+echo $dice2
+echo $dice3
+echo $dice4
+
+sum=$(($dice+$dice1+$dice2+$dice3+$dice4))
+avg=$(echo $sum/5)
+echo "sum of 5 RANDOM digit:"$sum
+echo "THE average of 5 RANDOM digit:"$avg
+~
+~
+~
+~
+~
 
