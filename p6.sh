413b964b3572143e5a3963a9efe3a4db1eabf853
Arul525 <100610009+Arul525@users.noreply.github.com>
2022-03-02 16:20:55 +0530

    Create p6.sh

---
+++ p6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "enter a number"
+read n
+case $n in
+        1) echo "sunday";;
+        2) echo "monday";;
+        3) echo "tuesday";;
+        4) echo "wed";;
+        5) echo "thu";;
+        6) echo "fri";;
+        7) echo "sat";;
+
+esac
+~
+~
+~
+~
+~
+~
