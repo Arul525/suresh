6b96c3613215841adfd36c36fee074c99712eb87
Arul525 <100610009+Arul525@users.noreply.github.com>
2022-03-02 16:04:27 +0530

    Update p2.sh

--- p2.sh
+++ p2.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+ispresent=2;
+
+x=$((RANDOM%2));
+if [ $ispresent -eq $x ]
+then
+        employeerateperhour=15;
+        emphour=8;
+        salary=$(($emphour*$employeerateperhour));
+else
+        salary=0;
+fi
+~
+~
