8fdb5e36209dab4f8b5ce0b0ffc0a23e78ce8474
Arul525 <100610009+Arul525@users.noreply.github.com>
2022-03-02 15:59:30 +0530

    Update p2.sh

--- p2.sh
+++ p2.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-echo "enter the with"
-read width
-echo "emter the height"
-read height
-plot=$(($width*$height))
-plot1=$(($plot*12))
-echo "one complete plot in inch :"$plot
-plot25=$(($plot1*25))
-echo "total 25 plor are:"$plot25
-~
-~
-~
-~
+
